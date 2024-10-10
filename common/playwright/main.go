31883b40c6cd727253fa52ab22289f22d30a5719
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-10-10 15:08:40 +0800

    fix: 优化 defer

--- common/playwright/main.go
+++ common/playwright/main.go
@@ -14,33 +14,30 @@ func ExecutePlaywright(cfg config.Config) {
 	if err != nil {
 		log.Fatalf("Playwright 初始化失败: %v", err)
 	}
-	defer browser.Close()
-	defer pw.Stop()
-	log.Println("Playwright 和浏览器初始化成功")
+	// 合并 defer 语句，统一处理关闭浏览器和停止 Playwright 的操作
+	defer func() {
+		browser.Close()
+		pw.Stop()
+	}()
 
 	context, err := browser.NewContext()
 	if err != nil {
 		log.Fatalf("创建浏览器上下文失败: %v", err)
 	}
 	defer context.Close()
-	log.Println("浏览器上下文创建成功")
 
 	if err := cookies.LoadCookies(context, cfg.CookiesFilePath); err != nil {
 		log.Fatalf("加载Cookies失败: %v", err)
 	}
-	log.Println("Cookies 加载成功")
 
 	page, err := context.NewPage()
 	if err != nil {
 		log.Fatalf("创建页面失败: %v", err)
 	}
 	defer page.Close()
-	log.Println("页面创建成功")
 
 	if _, err := page.Goto("https://www.zhihu.com/creator/manage/creation/article"); err != nil {
 		log.Fatalf("导航到页面失败: %v", err)
-	} else {
-		log.Println("成功导航到指定页面")
 	}
 
 	data, err := scraper.ExtractData(page)
