9b30f9584e54e609884bc957f9faabff799992c1
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-10-10 15:05:26 +0800

    fix: 提取无头浏览器爬取方式

--- common/playwright/main.go
+++ common/playwright/main.go
@@ -1,27 +1,52 @@
 package playwright
 
 import (
+	"crawler/common/scraper"
+	"crawler/config"
+	"crawler/cookies"
+	"fmt"
 	"log"
-
-	"github.com/playwright-community/playwright-go"
 )
 
-func InitializePlaywright() (*playwright.Playwright, playwright.Browser, error) {
-	log.Println("开始安装 Playwright")
-	pw, err := playwright.Run()
+// ExecutePlaywright 封装了使用Playwright进行网页爬取的逻辑
+func ExecutePlaywright(cfg config.Config) {
+	pw, browser, err := InitializePlaywright()
 	if err != nil {
-		return nil, nil, err
+		log.Fatalf("Playwright 初始化失败: %v", err)
 	}
-	log.Println("Playwright 启动成功")
+	defer browser.Close()
+	defer pw.Stop()
+	log.Println("Playwright 和浏览器初始化成功")
 
-	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
-		Headless: playwright.Bool(false),
-	})
+	context, err := browser.NewContext()
 	if err != nil {
-		pw.Stop()
-		return nil, nil, err
+		log.Fatalf("创建浏览器上下文失败: %v", err)
 	}
-	log.Println("浏览器启动成功")
+	defer context.Close()
+	log.Println("浏览器上下文创建成功")
 
-	return pw, browser, nil
+	if err := cookies.LoadCookies(context, cfg.CookiesFilePath); err != nil {
+		log.Fatalf("加载Cookies失败: %v", err)
+	}
+	log.Println("Cookies 加载成功")
+
+	page, err := context.NewPage()
+	if err != nil {
+		log.Fatalf("创建页面失败: %v", err)
+	}
+	defer page.Close()
+	log.Println("页面创建成功")
+
+	if _, err := page.Goto("https://www.zhihu.com/creator/manage/creation/article"); err != nil {
+		log.Fatalf("导航到页面失败: %v", err)
+	} else {
+		log.Println("成功导航到指定页面")
+	}
+
+	data, err := scraper.ExtractData(page)
+	if err != nil {
+		log.Fatalf("数据提取失败: %v", err)
+	}
+	fmt.Println(data)
+	log.Println("数据提取完成")
 }
